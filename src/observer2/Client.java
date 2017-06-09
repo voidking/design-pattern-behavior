fc4eab14c220ce055027d67b59d594a2912b95d9
voidking <voidking@qq.com>
2017-06-09 11:17:33 +0800

    update observer

--- src/observer2/Client.java
+++ src/observer2/Client.java
@@ -5,8 +5,12 @@ public class Client
     public static void main(String[] args)
     {
         Stock stock = new Stock();
-        stock.add(new AndroidClient());
-        stock.add(new ComputerClient());
+        StockObserver androidClient = new AndroidClient();
+        StockObserver computerClient = new ComputerClient();
+        stock.add(androidClient);
+        stock.add(computerClient);
+        
+        //stock.remove(androidClient);
         stock.changePrice();
         stock.changeCount();
     }
